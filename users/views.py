ce48ef3f0ff5f53e9bc9d09d3d9c7aaf6c9b83e4
Kwan Jeon <jeonkwan100@gmail.com>
2024-04-18 12:09:47 +0900

    index bootstrap

--- users/views.py
+++ users/views.py
@@ -3,13 +3,16 @@ from django.views.decorators.http import require_POST
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from products.models import Product
 
 
 @login_required
 def profile(request, username):
     member = get_object_or_404(get_user_model(), username=username)
+    articles = Product.objects.all().order_by('-created_at')
     context = {
         "member": member,
+        "articles": articles,
     }
     return render(request, "users/profile.html", context)
 
